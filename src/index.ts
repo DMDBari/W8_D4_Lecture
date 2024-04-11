eaaf5e8ea07088425160626f5e34981368cc4ab0
Dillon Dains <dilloncsbb@gmail.com>
2024-04-11 13:03:48 -0500

    initial commit with webpack, ts, and liveserver

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+console.log('Hello World!')
+console.log('Log another line')
+
+function sayHello(name:string){
+    console.log("hello " + name +'!')
+}
+
+sayHello('Dillon');
+sayHello('Ed')
+sayHello('Billy')
+
+console.log('Making a change')
